9e6c6a421a6aaf31bbbc156ee0d5904e6e67f8e0
MrChen <MrChen@guigu.com>
2021-12-28 19:14:20 +0800

    hot-fix first commit

---
+++ git-test/src/main/java/com/atguigu/Main.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author MrChen
+ * @create 2021-12-28 19:12
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello  world");
+    }
+}
