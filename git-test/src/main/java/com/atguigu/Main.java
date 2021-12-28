9b9be32d7e8ac51e7b7166701466f3c45c83c0cf
poinner02 <55753056+poinner02@users.noreply.github.com>
2021-12-28 19:50:13 +0800

    your code  is booshit

--- git-test/src/main/java/com/atguigu/Main.java
+++ git-test/src/main/java/com/atguigu/Main.java
@@ -6,6 +6,6 @@ package com.atguigu;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello  world");
+        System.out.println("fist push commit hello  world");
     }
 }
