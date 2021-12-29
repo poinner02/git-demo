aa6b32ea83ea29293a1f079ccc989190f23dcd93
MrChen <MrChen@guigu.com>
2021-12-29 20:46:47 +0800

    gitee config-server ver  client  broadcast  1.0.2

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/CofigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/CofigClientController.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2021-12-29 20:27
  */
 @RestController
+@RefreshScope
 public class CofigClientController {
 
     @Value("${config.info}")
